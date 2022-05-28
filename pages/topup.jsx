c2ce1bfea8c974a3b6c2d2757adc7c7032b189ad
Thariq Farsha <mthariqfarsha@gmail.com>
2022-05-28 18:03:17 +0700

    Slice topup page

---
+++ pages/topup.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MainLayout from "../components/Layout/MainLayout";
+
+export default function Topup() {
+  return (
+    <MainLayout title={"Top Up | FazzPay"}>
+      <div className="bg-white rounded shadow p-4 h-100 position-relative"></div>
+    </MainLayout>
+  );
+}
