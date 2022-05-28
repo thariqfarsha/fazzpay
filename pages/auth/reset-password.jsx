6040e1dfcdb1b54b3fc0cca601f0fb9c8711d781
Thariq Farsha <mthariqfarsha@gmail.com>
2022-05-28 18:02:39 +0700

    Create forgot and reset password page

---
+++ pages/auth/reset-password.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import Image from "next/image";
+import authImg from "../../public/images/auth-img.png";
+import Layout from "../../components/Layout/AuthLayout";
+
+export default function ResetPassword() {
+  return (
+    <Layout title={"Reset Password | FazzPay"}>
+      <div className="container-lg px-5 py-4 vh-100">
+        <div className="row h-100 shadow rounded border">
+          <div
+            className="col-7 rounded-start h-100 position-relative overflow-hidden"
+            style={{
+              backgroundImage: "url(/images/auth-bg-img.png)",
+            }}
+          >
+            <h1 className="h2 fw-bold text-white px-5 py-4 position-absolute top-0 start-0">
+              FazzPay
+            </h1>
+            <div
+              className="position-absolute start-50 translate-middle-x"
+              style={{ width: "59%", height: "auto", top: "12%" }}
+            >
+              <Image src={authImg} alt="login page illustration" />
+            </div>
+            <div className="px-5 py-4 position-absolute bottom-0 start-0">
+              <h2 className="h5 fw-bold text-white">
+                App that Covering Banking Needs.
+              </h2>
+              <p className="text-white opacity-75 m-0">
+                FazzPay is an application that focussing in banking needs for
+                all users in the world. Always updated and always following
+                world trends. 5000+ users registered in FazzPay everyday with
+                worldwide users coverage.
+              </p>
+            </div>
+          </div>
+          <div className="col-5 px-5 d-flex justify-content-center align-items-center">
+            <div>
+              <h2 className="h4 fw-bold mb-2">
+                Did You Forgot Your Password? Don’t Worry, You Can Reset Your
+                Password In a Minutes.
+              </h2>
+              <p className="opacity-75 mb-4">
+                Now you can create a new password for your Zwallet account. Type
+                your password twice so we can confirm your new passsword.
+              </p>
+              <form>
+                <div className="input-with-icon mb-3">
+                  <i className="bi bi-lock input-icon text-secondary"></i>
+                  <label
+                    for="new-password"
+                    className="form-label visually-hidden"
+                  >
+                    New Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="new-password"
+                    name="newPassword"
+                    placeholder="Enter your new password"
+                  />
+                </div>
+                <div className="input-with-icon mb-3">
+                  <i className="bi bi-lock input-icon text-secondary"></i>
+                  <label
+                    for="confirm-password"
+                    className="form-label visually-hidden"
+                  >
+                    Confirm Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="confirm-password"
+                    name="confirmPassword"
+                    placeholder="Confirm your new password"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-primary fw-bold mt-4 mb-5 w-100"
+                  disabled
+                >
+                  Reset Password
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
