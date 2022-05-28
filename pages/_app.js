4f5b00efadaf708d44d067ae198f03ba0a0eeee5
Thariq Farsha <mthariqfarsha@gmail.com>
2022-05-28 16:38:29 +0700

    import custom bootstrap adn bootstrap icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../scss/customBootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
