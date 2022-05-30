e823d3690615d9ca6aabd460eb5a2f8ae616bfc1
Thariq Farsha <mthariqfarsha@gmail.com>
2022-05-30 10:30:12 +0700

    Create pin input component

---
+++ components/PinInput/index.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+export default function PinInput(props) {
+  const addPin = (e) => {
+    if (e.target.value) {
+      const nextSibling = document.getElementById(
+        `pin-${parseInt(e.target.name, 10) + 1}`
+      );
+
+      if (nextSibling !== null) {
+        nextSibling.focus();
+      }
+    }
+    props.setPin({ ...props.pin, [`pin${e.target.name}`]: e.target.value });
+  };
+
+  return (
+    <div className="row row-cols-6 gx-3">
+      {Object.keys(props.pin).map((digit, index) => (
+        <div className="col" key={index}>
+          <label htmlFor="pin" className="form-label visually-hidden">
+            Input PIN Digit {`${index + 1}`}
+          </label>
+          <input
+            type="text"
+            pattern="\d"
+            id={`pin-${index + 1}`}
+            name={`${index + 1}`}
+            className="form-control text-center text-primary px-0 fw-bold fs-5 mb-3"
+            maxLength={1}
+            onChange={addPin}
+            autoComplete="off"
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
