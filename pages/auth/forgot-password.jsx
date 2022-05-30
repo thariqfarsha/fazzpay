12a453a93723d1e368dc241ebc64ec2e8246ad2e
Thariq Farsha <mthariqfarsha@gmail.com>
2022-05-30 10:30:41 +0700

    Integrate forgot pass page with backend

--- pages/auth/forgot-password.jsx
+++ pages/auth/forgot-password.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import authImg from "../../public/images/auth-img.png";
 import Layout from "../../components/Layout/AuthLayout";
+import axios from "../../utils/axios";
 
 export default function ForgotPassword() {
+  const [message, setMessage] = useState("");
+  const [formForgotPass, setFormForgotPass] = useState({
+    email: "",
+    linkDirect: "http://localhost:3000/auth/reset-password",
+  });
+
+  const handleChangeForm = (e) => {
+    const { name, value } = e.target;
+    setFormForgotPass({ ...formForgotPass, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const result = await axios.post("/auth/forgot-password", formForgotPass);
+      setMessage(result.data.msg);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Layout title={"Forgot Password | FazzPay"}>
       <div className="container-lg px-5 py-4 vh-100">
@@ -35,21 +57,28 @@ export default function ForgotPassword() {
               </p>
             </div>
           </div>
-          <div className="col-5 px-5 d-flex justify-content-center align-items-center">
+          <div className="col-5 p-5">
             <div>
               <h2 className="h4 fw-bold mb-2">
                 Did You Forgot Your Password? Don't Worry, You Can Reset Your
                 Password In a Minutes.
               </h2>
-              <p className="opacity-75 mb-4">
+              <p className="opacity-75">
                 To reset your password, you must type your e-mail and we will
                 send a link to your email and you will be directed to the reset
                 password screens.
               </p>
-              <form>
-                <div className="input-with-icon mb-3">
+              {message ? (
+                <div className="alert alert-success py-2" role="alert">
+                  {message}
+                </div>
+              ) : (
+                <div className="mt-5"></div>
+              )}
+              <form onSubmit={handleSubmit}>
+                <div className="input-with-icon mb-4">
                   <i className="bi bi-envelope input-icon text-secondary"></i>
-                  <label for="email" className="form-label visually-hidden">
+                  <label htmlFor="email" className="form-label visually-hidden">
                     Email
                   </label>
                   <input
@@ -58,12 +87,11 @@ export default function ForgotPassword() {
                     id="email"
                     name="email"
                     placeholder="Enter your email"
+                    value={formForgotPass.email}
+                    onChange={handleChangeForm}
                   />
                 </div>
-                <button
-                  type="submit"
-                  className="btn btn-primary fw-bold mt-4 mb-5 w-100"
-                >
+                <button type="submit" className="btn btn-primary fw-bold w-100">
                   Confirm
                 </button>
               </form>
