a02a9012eb180f03d165d9064177a7fe855af2fd
Thariq Farsha <mthariqfarsha@gmail.com>
2022-05-30 10:33:02 +0700

    Change reset-password path, integrate page w/ backend

--- pages/auth/reset-password/[keys].jsx
+++ pages/auth/reset-password/[keys].jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
-import authImg from "../../public/images/auth-img.png";
-import Layout from "../../components/Layout/AuthLayout";
+import authImg from "../../../public/images/auth-img.png";
+import Layout from "../../../components/Layout/AuthLayout";
+import { useRouter } from "next/router";
+import axios from "../../../utils/axios";
 
 export default function ResetPassword() {
+  const router = useRouter();
+  const keys = router.query.keys;
+
+  const [isError, setIsError] = useState(false);
+  const [message, setMessage] = useState("");
+  const [formNewPassword, setFormNewPassword] = useState({
+    keysChangePassword: keys,
+    newPassword: "",
+    confirmPassword: "",
+  });
+
+  const handleChangeForm = (e) => {
+    const { name, value } = e.target;
+    setFormNewPassword({ ...formNewPassword, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      setFormNewPassword({ ...formNewPassword, keysChangePassword: keys });
+      const result = await axios.patch("/auth/reset-password", formNewPassword);
+      console.log(result);
+      setIsError(false);
+      setMessage(result.data.data.msg);
+    } catch (error) {
+      console.log(error);
+      setIsError(true);
+      setMessage(error.response.data.msg);
+    }
+  };
+
   return (
     <Layout title={"Reset Password | FazzPay"}>
       <div className="container-lg px-5 py-4 vh-100">
@@ -35,21 +68,34 @@ export default function ResetPassword() {
               </p>
             </div>
           </div>
-          <div className="col-5 px-5 d-flex justify-content-center align-items-center">
+          <div className="col-5 p-5">
             <div>
               <h2 className="h4 fw-bold mb-2">
-                Did You Forgot Your Password? Don’t Worry, You Can Reset Your
+                Did You Forgot Your Password? Don't Worry, You Can Reset Your
                 Password In a Minutes.
               </h2>
-              <p className="opacity-75 mb-4">
+              <p className="opacity-75">
                 Now you can create a new password for your Zwallet account. Type
                 your password twice so we can confirm your new passsword.
               </p>
-              <form>
+              {message ? (
+                isError ? (
+                  <div className="alert alert-danger py-2" role="alert">
+                    {message}
+                  </div>
+                ) : (
+                  <div className="alert alert-success py-2" role="alert">
+                    {message}
+                  </div>
+                )
+              ) : (
+                <div className="mb-4"></div>
+              )}
+              <form onSubmit={handleSubmit}>
                 <div className="input-with-icon mb-3">
                   <i className="bi bi-lock input-icon text-secondary"></i>
                   <label
-                    for="new-password"
+                    htmlFor="new-password"
                     className="form-label visually-hidden"
                   >
                     New Password
@@ -60,12 +106,15 @@ export default function ResetPassword() {
                     id="new-password"
                     name="newPassword"
                     placeholder="Enter your new password"
+                    value={formNewPassword.newPassword}
+                    onChange={handleChangeForm}
+                    required
                   />
                 </div>
-                <div className="input-with-icon mb-3">
+                <div className="input-with-icon mb-4">
                   <i className="bi bi-lock input-icon text-secondary"></i>
                   <label
-                    for="confirm-password"
+                    htmlFor="confirm-password"
                     className="form-label visually-hidden"
                   >
                     Confirm Password
@@ -76,15 +125,28 @@ export default function ResetPassword() {
                     id="confirm-password"
                     name="confirmPassword"
                     placeholder="Confirm your new password"
+                    value={formNewPassword.confirmPassword}
+                    onChange={handleChangeForm}
+                    required
                   />
                 </div>
                 <button
                   type="submit"
-                  className="btn btn-primary fw-bold mt-4 mb-5 w-100"
-                  disabled
+                  className={`btn ${
+                    isError ? "btn-outline-primary" : "btn-primary"
+                  } fw-bold w-100`}
                 >
                   Reset Password
                 </button>
+                {isError ? (
+                  <button
+                    type="button"
+                    className="btn btn-primary fw-bold w-100 mt-2"
+                    onClick={() => router.push("/auth/forgot-password")}
+                  >
+                    Forgot Password
+                  </button>
+                ) : null}
               </form>
             </div>
           </div>
