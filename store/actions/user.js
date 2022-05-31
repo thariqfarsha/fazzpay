e420ac5ed12cff0c5d606e0deae827c306097f54
Thariq Farsha <mthariqfarsha@gmail.com>
2022-05-31 16:05:57 +0700

    Change redux user action name

--- store/actions/user.js
+++ store/actions/user.js
@@ -1,13 +1,13 @@
 import axios from "../../utils/axios";
 
-export const getUserById = (id) => {
+export const getUserByIdRedux = (id) => {
   return {
     type: "GET_USER_BY_ID",
     payload: axios.get(`/user/profile/${id}`),
   };
 };
 
-export const logout = () => {
+export const logoutRedux = () => {
   return {
     type: "LOGOUT",
     payload: axios.post("/auth/logout"),
