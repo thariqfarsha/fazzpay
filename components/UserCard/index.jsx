baaf0085c65e2613871f18a73dbca90939e039e4
Thariq Farsha <mthariqfarsha@gmail.com>
2022-06-02 02:27:04 +0700

    Fix image on user card

--- components/UserCard/index.jsx
+++ components/UserCard/index.jsx
@@ -6,10 +6,17 @@ export default function UserCard(props) {
   const { firstName, lastName, noTelp, image } = props.data;
 
   return (
-    <div className="rounded shadow-sm p-3 d-flex justify-content-between align-items-center">
+    <div className="rounded shadow-sm p-2 d-flex justify-content-between align-items-center">
       <div className="d-flex">
         <div className="rounded me-3" style={{ width: "48px", height: "48px" }}>
-          <Image src={image ? image : blankImage} alt="profile picture" />
+          <Image
+            src={image ? process.env.URL_CLOUDINARY + image : blankImage}
+            alt="profile picture"
+            width={48}
+            height={48}
+            objectFit="cover"
+            style={{ borderRadius: "12px" }}
+          />
         </div>
         <div>
           <p className="fw-bold mb-1">{firstName + " " + lastName}</p>
