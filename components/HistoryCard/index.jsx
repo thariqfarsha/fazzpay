2a641b95b3c2b3a7903e0fb2cc6bc8c383ac5574
Thariq Farsha <mthariqfarsha@gmail.com>
2022-06-02 02:24:36 +0700

    Integrate history page

--- components/HistoryCard/index.jsx
+++ components/HistoryCard/index.jsx
@@ -1,27 +1,43 @@
 import React from "react";
 import Image from "next/image";
+import blankProfile from "../../public/profiles/blank.png";
+import currency from "../../utils/currency";
+import { useRouter } from "next/router";
 
 export default function HistoryCard(props) {
-  const { fullName, type, image, amount } = props.data;
+  const router = useRouter();
+  const path = router.asPath;
+  console.log(router);
+
+  const { firstName, fullName, type, image, amount } = props.data;
 
   return (
-    <div className="bg-white mb-3 d-flex justify-content-between align-items-center">
+    <div className="bg-white mb-4 d-flex justify-content-between align-items-center">
       <div className="d-flex">
         <div className="rounded me-3" style={{ width: "48px", height: "48px" }}>
-          <Image src={image} alt="profile picture" />
+          <Image
+            src={image ? process.env.URL_CLOUDINARY + image : blankProfile}
+            alt="profile picture"
+            width={48}
+            height={48}
+            objectFit="cover"
+            style={{ borderRadius: "10px" }}
+          />
         </div>
         <div>
-          <p className="fw-bold mb-1">{fullName}</p>
+          <p className="fw-bold mb-1 text-truncate">
+            {path === "/dashboard" ? firstName : fullName}
+          </p>
           <p className="opacity-75 fs-7 m-0">{type}</p>
         </div>
       </div>
       <p
         className={`fw-bold m-0 ${
-          type === "Accept" ? "text-success" : "text-danger"
+          type === "accept" || type === "topup" ? "text-success" : "text-danger"
         }`}
       >
-        {type === "Accept" ? "+" : "-"}
-        {amount}
+        {type === "accept" || type === "topup" ? "+" : "-"}
+        {currency.format(amount)}
       </p>
     </div>
   );
--- pages/history.jsx
+++ pages/history.jsx
@@ -1,103 +1,107 @@
 import React from "react";
 import Layout from "../components/Layout/MainLayout";
-import blankProfile from "../public/profiles/blank.png";
 import HistoryCard from "../components/HistoryCard";
+import cookies from "next-cookies";
+import axiosServer from "../utils/axiosServer";
+import ReactPaginate from "react-paginate";
+import { useRouter } from "next/router";
 
-export default function History() {
-  const histories = [
-    {
-      id: 1,
-      fullName: "Samuel Suhi",
-      type: "Accept",
-      image: blankProfile,
-      amount: 50000,
-    },
-    {
-      id: 2,
-      fullName: "Netflix",
-      type: "Transfer",
-      image: blankProfile,
-      amount: 149000,
-    },
-    {
-      id: 3,
-      fullName: "Christine Martha",
-      type: "Accept",
-      image: blankProfile,
-      amount: 150000,
-    },
-    {
-      id: 4,
-      fullName: "Adobe",
-      type: "Transfer",
-      image: blankProfile,
-      amount: 249000,
-    },
-    {
-      id: 5,
-      fullName: "Netflix",
-      type: "Transfer",
-      image: blankProfile,
-      amount: 149000,
-    },
-    {
-      id: 6,
-      fullName: "Christine Martha",
-      type: "Accept",
-      image: blankProfile,
-      amount: 150000,
-    },
-    {
-      id: 7,
-      fullName: "Adobe",
-      type: "Transfer",
-      image: blankProfile,
-      amount: 249000,
-    },
-    {
-      id: 5,
-      fullName: "Netflix",
-      type: "Transfer",
-      image: blankProfile,
-      amount: 149000,
-    },
-    {
-      id: 6,
-      fullName: "Christine Martha",
-      type: "Accept",
-      image: blankProfile,
-      amount: 150000,
-    },
-    {
-      id: 7,
-      fullName: "Adobe",
-      type: "Transfer",
-      image: blankProfile,
-      amount: 249000,
-    },
-  ];
+export async function getServerSideProps(context) {
+  try {
+    const dataCookies = cookies(context);
+    const params = context.query;
+    const page = !params?.page ? 1 : params.page;
+    const limit = !params?.limit ? 5 : params.limit;
+    const filter = !params?.filter ? "" : params.filter;
+    const result = await axiosServer.get(
+      `/transaction/history?page=${page}&limit=${limit}&filter=${filter}`,
+      {
+        headers: {
+          Authorization: `Bearer ${dataCookies.token}`,
+        },
+      }
+    );
+    return {
+      props: {
+        data: result.data.data,
+        pagination: result.data.pagination,
+        filter: filter,
+      },
+    };
+  } catch (error) {
+    return {
+      redirect: {
+        destination:
+          error.response?.status === 403
+            ? "/auth/login"
+            : `/error?msg=${error.response?.data.msg}`,
+        permanent: false,
+      },
+    };
+  }
+}
+
+export default function History(props) {
+  const router = useRouter();
+
+  const histories = props.data;
+  const pagination = props.pagination;
+  const filter = props.filter;
+
+  const handleChangeFilter = (e) => {
+    router.push(`/history?page=${pagination.page}&filter=${e.target.value}`);
+  };
+
+  const handlePagination = (e) => {
+    router.push(
+      `/history?page=${e.selected + 1}${filter ? `&filter=${filter}` : ""}`
+    );
+  };
 
   return (
     <Layout title={"Transaction History | FazzPay"}>
-      <div className="bg-white rounded shadow p-4 h-100 overflow-hidden">
+      <div className="bg-white rounded shadow p-4 h-100 overflow-hidden position-relative">
         <div className="mb-4 d-flex justify-content-between align-items-center">
           <h2 className="fs-5 fw-bold">Transaction History</h2>
           <select
             className="form-select bg-secondary bg-opacity-25 border-0 w-25"
             aria-label="transaction history filter"
+            onChange={handleChangeFilter}
           >
-            <option defaultValue={""}>-- Select Filter --</option>
+            <option defaultValue={""} value="">
+              -- Select Filter --
+            </option>
             <option value="week">Week</option>
             <option value="month">Month</option>
             <option value="year">Year</option>
           </select>
         </div>
-        <div className="scrollable-wrapper" style={{ height: "85%" }}>
+        <div className="scrollable-wrapper" style={{ height: "75%" }}>
           {histories.map((history) => (
-            <div className="mb-4" key={history.id}>
+            <div key={history.id}>
               <HistoryCard data={history} />
             </div>
           ))}
+        </div>
+
+        <div className="d-flex justify-content-center position-absolute bottom-0 start-50 translate-middle-x">
+          <ReactPaginate
+            previousLabel={"Previous"}
+            nextLabel={"Next"}
+            breakLabel={"..."}
+            pageCount={pagination.totalPage}
+            onPageChange={handlePagination}
+            containerClassName={"pagination mb-4"}
+            pageClassName={"page-item px-1"}
+            pageLinkClassName={"page-link rounded"}
+            previousClassName={"page-item visually-hidden"}
+            previousLinkClassName={"page-link"}
+            nextClassName={"page-item visually-hidden"}
+            nextLinkClassName={"page-link"}
+            subContainerClassName={"pages pagination"}
+            activeClassName={"active"}
+            activeLinkClassName={"text-white shadow"}
+          />
         </div>
       </div>
     </Layout>
