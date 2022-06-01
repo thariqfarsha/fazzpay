01f11791eb0591cc773989f4f40fb91f32fc6bc5
Thariq Farsha <mthariqfarsha@gmail.com>
2022-06-02 02:22:55 +0700

    Integrate change password page

--- pages/profile/change-password.jsx
+++ pages/profile/change-password.jsx
@@ -1,36 +1,103 @@
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import MainLayout from "../../components/Layout/MainLayout";
+import axios from "../../utils/axios";
 
 export default function ChangePassword() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const router = useRouter();
+
+  const [formPassword, setFormPassword] = useState({
+    oldPassword: "",
+    newPassword: "",
+    confirmPassword: "",
+  });
+  const [isError, setIsError] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const userId = useSelector((state) => state.user.data.id);
+
+  useEffect(() => {
+    setIsError(false);
+    setMessage("");
+    resetForm();
+  }, []);
+
+  const handleChangeForm = (e) => {
+    const { name, value } = e.target;
+    setFormPassword({ ...formPassword, [name]: value });
+  };
+
+  const resetForm = () => {
+    setFormPassword({
+      oldPassword: "",
+      newPassword: "",
+      confirmPassword: "",
+    });
+  };
+
+  const handleUpdatePassword = async (e) => {
+    try {
+      e.preventDefault();
+      await axios.patch(`/user/password/${userId}`, formPassword);
+      setIsError(false);
+      setMessage("Password updated");
+      resetForm();
+    } catch (error) {
+      console.log(error);
+      setIsError(true);
+      setMessage(error.response.data.msg);
+    }
   };
 
   return (
     <MainLayout title={"Change Password | FazzPay"}>
       <div className="bg-white rounded shadow p-4 h-100 position-relative">
-        <h2 className="fs-5 fw-bold mb-3">Change Password</h2>
-        <p className="opacity-50">
+        <div className="d-flex align-items-center mb-3">
+          <button className="btn px-1 py-0 me-2" onClick={() => router.back()}>
+            <i className="bi bi-chevron-left"></i>
+          </button>
+          <h2 className="fs-5 fw-bold m-0">Change Password</h2>
+        </div>
+        <p className="opacity-50 w-50">
           You must enter your current password and then type your new password
           twice.
         </p>
-        <form onSubmit={handleSubmit} className="w-50 mx-auto mt-5 pt-3">
+        <form onSubmit={handleUpdatePassword} className="w-50 mx-auto pt-3">
+          {message ? (
+            isError ? (
+              <div className="alert alert-danger py-2" role="alert">
+                {message}
+              </div>
+            ) : (
+              <div className="alert alert-success py-2" role="alert">
+                {message}
+              </div>
+            )
+          ) : (
+            <div className="mt-4"></div>
+          )}
           <div className="input-with-icon mb-4">
             <i className="bi bi-key input-icon text-secondary"></i>
-            <label for="currentPassword" className="form-label visually-hidden">
+            <label
+              htmlFor="currentPassword"
+              className="form-label visually-hidden"
+            >
               Current Password
             </label>
             <input
               type="password"
               className="form-control"
               id="currentPassword"
-              name="currentPassword"
+              name="oldPassword"
               placeholder="Current password"
+              value={formPassword.oldPassword}
+              onChange={handleChangeForm}
             />
           </div>
           <div className="input-with-icon mb-4">
             <i className="bi bi-lock input-icon text-secondary"></i>
-            <label for="newPassword" className="form-label visually-hidden">
+            <label htmlFor="newPassword" className="form-label visually-hidden">
               New Password
             </label>
             <input
@@ -39,11 +106,16 @@ export default function ChangePassword() {
               id="newPassword"
               name="newPassword"
               placeholder="New password"
+              value={formPassword.newPassword}
+              onChange={handleChangeForm}
             />
           </div>
-          <div className="input-with-icon mb-5">
+          <div className="input-with-icon mb-4">
             <i className="bi bi-lock input-icon text-secondary"></i>
-            <label for="confirmPassword" className="form-label visually-hidden">
+            <label
+              htmlFor="confirmPassword"
+              className="form-label visually-hidden"
+            >
               Confirm Password
             </label>
             <input
@@ -52,6 +124,8 @@ export default function ChangePassword() {
               id="confirmPassword"
               name="confirmPassword"
               placeholder="Repeat your new password"
+              value={formPassword.confirmPassword}
+              onChange={handleChangeForm}
             />
           </div>
           <button type="submit" className="btn btn-primary fw-bold w-100">
