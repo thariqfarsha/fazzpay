a3a8be7b9421981a1397bfff89d2c959e6557fee
Thariq Farsha <mthariqfarsha@gmail.com>
2022-06-02 02:07:15 +0700

    Change js bootstrap: cdn -> import

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,15 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import Store from "../store";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    typeof document !== undefined
+      ? require("../node_modules/bootstrap/dist/js/bootstrap")
+      : null;
+  }, []);
+
   return (
     <Provider store={Store.store}>
       <PersistGate loading={null} persistor={Store.persistor}>
