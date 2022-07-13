775dda0db059c14d9e2c6924dae43cdb16687db9
Thariq Farsha <mthariqfarsha@gmail.com>
2022-07-13 17:17:43 +0700

    Add font weight variant from google fonts

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
             crossOrigin="true"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600;700;800;900&display=swap"
             rel="stylesheet"
           />
         </Head>
